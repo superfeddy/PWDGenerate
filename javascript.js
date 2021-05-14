ba9f56ecc5aac34eb9ad6d4d0009edfb0bd60472
superfeddy <fedf0415@gmail.com>
2021-05-14 15:57:35 -0700

    add comments to javascript

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 document.getElementById('generate')
 
 // When button is clicked, generate new password based on users desired character set and length.
-
 generate.addEventListener("click", function () {
 
-  // Receive users desired character sets. Ask them if the want the password to include lower case, upper case, numbers, and special characters.
+  // Ask user which character sets they'd like included in their password. Ask them if the want the password to include lower case, upper case, numbers, and special characters.
 
   let lowerCaseYes = confirm('Do you want to include lowercase characters? Click "Ok" for yes, "cancel" for no.')
 
@@ -20,17 +19,22 @@ generate.addEventListener("click", function () {
   let numChars = '0123456789'
   let specChars = '!@#$%^&*()_+{}:<>?,./;'
 
+  // chosenArray will be the array their selected character sets are added to. This is the array that will be randomly selected through.
   let chosenArray = []
 
+  // Add character sets to the chosenArray based on what they selected earlier.
   if (lowerCaseYes) { chosenArray += lowerChars }
   if (upperCaseYes) { chosenArray += upperChars }
   if (numsYes) { chosenArray += numChars }
   if (specialYes) { chosenArray += specChars }
 
-  let finalArray = chosenArray.split('')
+  // finalArray formats the array with commas between each character.
+  let formattedArray = chosenArray.split('')
 
+  // passwordGeneration is the array the randomly selected characters will be added to based on the user's chosen length.
   let passwordGeneration = []
 
+  // length asks the user how long they want their password. It limits the option to 8-128 and confirms that the user input is within that range. If it is not within that range, it prompts the user to enter a number within that range.
   let length = parseInt(prompt('How long do you want your password? Must be between 8 and 128 characters.'))
   while (length < 8 || length > 128 || length === NaN || length === null) {
     alert('Must be between 8 and 128 characters')
@@ -38,16 +42,17 @@ generate.addEventListener("click", function () {
     return length
   }
 
-
-  // Based on the length they chose, create 
+  // Now that we know how long they would like the password, this for loop randomly selects from the formattedArray and pushes that randomly selected character to passwordGeneration.
 
   for (i = 0; i < length; i++) {
-    passwordGeneration.push(finalArray[Math.floor(Math.random() * finalArray.length)])
+    passwordGeneration.push(formattedArray[Math.floor(Math.random() * formattedArray.length)])
   }
 
-  console.log(passwordGeneration.join(''))
+  // console.log to test it worked. Use join method to remove commas. Add final password via passwordGeneration to the text box on the html.
+  // console.log(passwordGeneration.join(''))
   document.getElementById('password').innerHTML = passwordGeneration.join('')
 
 }
 )
 
+// Tada!
